d8b97c95443820c66932e6ed063f8b565981e3f8
LGY2010 <15061555@qq.com>
2019-07-11 16:51:54 +0800

    test011

--- scripts/test01.py
+++ scripts/test01.py
@@ -6,7 +6,7 @@ class Test01():
 
     @allure.step(title="正在执行登录操作！")
     def test001(self):
-        print("test001被执行")
+        print("test0011  被执行")
 
     @allure.step(title="正在执行退出操作！")
     def test002(self):
